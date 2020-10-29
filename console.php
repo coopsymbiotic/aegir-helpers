0c99924b9772daea2cfa0fd7337374894a9e1aaa
Mathieu Lutfy <mathieu@symbiotic.coop>
2020-10-29 09:13:56 -0400

    Add missing zombie-database references in console.php

--- console.php
+++ console.php
@@ -5,7 +5,9 @@ require_once __DIR__ . '/vendor/autoload.php';
 
 use Symfony\Component\Console\Application;
 use Console\Command\AegirGitPull;
+use Console\Command\AegirZombieDatabases;
 
-$app = new Application('Aegir Helpers', 'v1.0.0');
+$app = new Application('Aegir Helpers', 'v1.1.0');
 $app->add(new AegirGitPull());
+$app->add(new AegirZombieDatabases());
 $app->run();
