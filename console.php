229bc4017eb69f36fc118f4510f82f8a35cd8242
Mathieu Lu <mathieu@symbiotic.coop>
2024-08-29 19:30:48 -0400

    Bump version 1.3.0

--- console.php
+++ console.php
@@ -5,7 +5,7 @@ require_once __DIR__ . '/vendor/autoload.php';
 
 use Symfony\Component\Console\Application;
 
-$app = new Application('Aegir Helpers', 'v1.2.0');
+$app = new Application('Aegir Helpers', 'v1.3.0');
 $app->add(new \Console\Command\AegirGitPull());
 $app->add(new \Console\Command\AegirZombieDatabases());
 $app->add(new \Console\Command\AegirZombieGrants());
